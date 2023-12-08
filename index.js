d6b5b8606d2261596daedf9a83bb4f02fca2aee0
Roizor <aidenjs9@icloud.com>
2023-12-07 23:46:02 -0600

    Use path resolve

--- index.js
+++ index.js
@@ -36,13 +36,13 @@ const abr = {
 	extract: (filename) => {
 		return new Promise((resolve, reject) => {
 			if (!fs.existsSync('./' + abr._temporaryDir)) fs.mkdirSync('./' + abr._temporaryDir);
-			fs.chmodSync('./' + abr._temporaryDir, 0o777);
+			fs.chmodSync(path.resolve('./' + abr._temporaryDir), 0o777);
 			asar.extractAll(`./${filename}`, `${abr._temporaryDir}/${abr._extrPrefix}${filename}`);
-			abr._watch(`./${abr._temporaryDir}/${abr._extrPrefix}${filename}/package.json`,2).then(() => {
-				fs.chmodSync(`./${abr._temporaryDir}/${abr._extrPrefix}${filename}`, 0o777);
-				const mainCfgFile = require(`./${abr._temporaryDir}/${abr._extrPrefix}${filename}/package.json`).main;
-				const cfg = require(`./${abr._temporaryDir}/${abr._extrPrefix}` + filename + '/' + mainCfgFile);
-				const module = require(`./${abr._temporaryDir}/${abr._extrPrefix}${filename}/` + cfg.entrypoint);
+			abr._watch(path.resolve(`./${abr._temporaryDir}/${abr._extrPrefix}${filename}/package.json`),2).then(() => {
+				fs.chmodSync(path.resolve(`./${abr._temporaryDir}/${abr._extrPrefix}${filename}`), 0o777);
+				const mainCfgFile = require(path.resolve(`./${abr._temporaryDir}/${abr._extrPrefix}${filename}/package.json`)).main;
+				const cfg = require(path.resolve(`./${abr._temporaryDir}/${abr._extrPrefix}` + filename + '/' + mainCfgFile));
+				const module = require(path.resolve(`./${abr._temporaryDir}/${abr._extrPrefix}${filename}/` + cfg.entrypoint));
 				abr.modules.set(filename, module);
 				resolve(filename);
 			})
